15929e4baf5f57eb295460ddeb149908c46cd91a
Seung-Yoon Oh <feynubrick@gmail.com>
2021-04-25 17:20:51 +0900

    added APIs to get titles of posts and a title of a post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, post_list, post_detail
 
 urlpatterns = [
     path('', index, name='index'),
+    path('posts', post_list, name='post_list'),
+    path('posts/<int:post_id>', post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+
+from .models import Post
 
 # Create your views here.
-def index(request):
+def index(request: HttpRequest):
     return HttpResponse('OK')
+
+def post_list(request: HttpRequest):
+    post_queryset = Post.objects.order_by('id')
+    titles = [str(post) for post in post_queryset]
+    title_list_text = ','.join(titles)
+    return HttpResponse(title_list_text)
+
+def post_detail(request: HttpRequest, post_id: int):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return HttpResponse('Post does not exist!', status=404)
+    
+    return HttpResponse(str(post))
